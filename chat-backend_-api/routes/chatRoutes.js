0968d24a2a8547a4b4f09cc87a2f3cbd2bc526b6
Akhil Nishad <akhilnishadofficial@gmail.com>
2024-03-17 15:48:13 +0530

    deleted backend code

--- chat-backend_-api/routes/chatRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const chatController = require('../controllers/chatController');
-
-router.post('/connect', chatController.connect);
-router.post('/chat', chatController.chat);
-router.post('/disconnect', chatController.disconnect);
-
-module.exports = router;
