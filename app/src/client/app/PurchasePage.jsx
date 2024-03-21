0660ee9675266b7d1f0ee12dbc64db6e53463c90
Akhil Nishad <akhilnishadofficial@gmail.com>
2024-03-21 09:36:49 +0530

    placeholder for purchase page

--- app/src/client/app/PurchasePage.jsx
+++ app/src/client/app/PurchasePage.jsx
@@ -39,20 +39,37 @@ export const PurchasePage = ({ user }) => {
         today.
       </p>
       <div className='flex '>
-        <Card className='p-5 flex flex-col gap-5 items-center max-w-[200px]  mt-10 mx-auto'>
-          <div>Purchase Credits to continue chat.</div>
-          <Button onClick={() => handleChangeCredit(10)}>
-            Get more credits
+        <Card className='p-5 flex flex-col items-center min-w-[350px]  mt-10 mx-auto'>
+          <h1 className='text-xl font-bold mb-1'>Purchase Credits </h1>
+          <div className='text-xs text-red-500'>* used for AI chat only.</div>
+          <div className='flex flex-col gap-5 items-center justify-center'>
+            <h2 className='text-lg font-bold mt-3'>Credit options</h2>
+            <div>10 credits</div>
+            <div className='h-[1px] w-full bg-gray-300' />
+            <div>20 credits</div>
+            <div className='h-[1px] w-full bg-gray-300' />
+            <div>30 credits</div>
+          </div>
+          <Button className='mt-5' onClick={() => handleChangeCredit(10)}>
+            Purchase
           </Button>
         </Card>
-        <Card className='p-5 flex flex-col gap-5 items-center max-w-[200px]  mt-10 mx-auto'>
-          <div>Purchase Premium Plan.</div>
+        <Card className='p-5 flex flex-col gap-5 items-center min-w-[350px]  mt-10 mx-auto justify-between'>
+          <h1 className='text-xl font-bold'>Premium Plan.</h1>
+          <div className='flex flex-col gap-5 items-center justify-center'>
+            <h2 className='text-lg font-bold'>Features</h2>
+            <div>20 free credits</div>
+            <div>Unlimited access to chartered accountants.</div>
+          </div>
           <Button onClick={handleSubscribe} disabled={user.subscriptionStatus}>
             {user.subscriptionStatus ? 'Already subscribed' : 'Subscribe'}
           </Button>
         </Card>
       </div>
-      <Button className='mt-5 mx-auto' onClick={() => window.history.back()}>
+      <Button
+        className='mt-[10svh] mx-auto'
+        onClick={() => window.history.back()}
+      >
         Back
       </Button>
     </section>
