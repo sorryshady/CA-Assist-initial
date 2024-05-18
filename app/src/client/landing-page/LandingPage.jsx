4347175abce04c4b7654ef7e089859634e7a101a
Akhil Nishad <akhilnishadofficial@gmail.com>
2024-05-18 12:56:09 +0530

    optimised landing page

--- app/src/client/landing-page/LandingPage.jsx
+++ app/src/client/landing-page/LandingPage.jsx
@@ -16,7 +16,7 @@ export const LandingPage = () => {
   return (
     <main className='w-full h-[90svh] flex items-center justify-center overflow-hidden'>
       <DotGrid />
-      <div className='w-full flex-1 flex flex-col justify-center gap-5 p-5 z-10'>
+      <div className='w-full flex-1 flex flex-col justify-center gap-5 p-5 z-10 bg-white lg:bg-transparent'>
         <h1 className='text-4xl md:text-3xl  font-semibold'>
           Need expert advice on taxes?
         </h1>
